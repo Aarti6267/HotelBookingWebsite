1be27cd0b531f115bb719f2436b658c7bd336bac
Aarti6267 <97041332+Aarti6267@users.noreply.github.com>
2022-04-06 13:29:37 +0530

    Checkpoint 11

--- detail.js
+++ detail.js
@@ -1,1 +1,21 @@
 //These files are empty now....
+
+function myFunction() {
+    var adult = document.getElementById("inputAdult").value ;
+    var toDate = document.getElementById("inputToDate").value;
+    var fromDate = document.getElementById("inputFromDate").value;
+    var total = +adult * +(fromDate-toDate);
+    document.getElementById("inputTotal").innerHTML = total;
+
+}
+
+function myFunction(){
+    var adult= document.getElementById("inputAdult").value;
+    var toDate= document.getElementById("inputToDate").value;
+    var fromDate = document.getElementById("inputFromDate").value;
+    const dateone=new date(d1);
+    const datetwo=new date(d2);
+    const time=Math.abs(datetwo-dateone);
+    const days=Math.ceil(time/(1000*60*60*24));
+    document.getElementById('inputTotal').value=1000*days*adult;
+}
